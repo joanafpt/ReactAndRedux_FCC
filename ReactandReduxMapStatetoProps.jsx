cc315d31023077ea0b9dfd3dc9bbc570f8adaacf
Joana <joanajteixeira@outlook.com>
2019-05-30 15:45:17 +0100

    React and Redux: Map State to Props
    /*Create a function mapStateToProps(). This function should take state as an argument, then return an object which maps that state to specific property names. These properties will become accessible to your component via props. Since this example keeps the entire state of the app in a single array, you can pass that entire state to your component. Create a property messages in the object that's being returned, and set it to state. */

---
+++ ReactandReduxMapStatetoProps.jsx
@@ -0,0 +1,14 @@
+//React and Redux: Map State to Props
+/*Create a function mapStateToProps(). This function should take state as an argument, then return an object which maps that state to specific property names. These properties will become accessible to your component via props. Since this example keeps the entire state of the app in a single array, you can pass that entire state to your component. Create a property messages in the object that's being returned, and set it to state. */
+
+
+const state = [];
+
+// change code below this line
+
+function mapStateToProps(state) {
+  let ob = {
+    messages: state
+  }
+  return ob;
+}
