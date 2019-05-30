d275f1af13c9e667c59c2974582c7f6650626b31
Joana <joanajteixeira@outlook.com>
2019-05-30 15:44:18 +0100

    React and Redux: Extract State Logic to Redux
    /*First, define an action type 'ADD' and set it to a const ADD. Next, define an action creator addMessage() which creates the action to add a message. You'll need to pass a message to this action creator and include the message in the returned action.
    Then create a reducer called messageReducer() that handles the state for the messages. The initial state should equal an empty array. This reducer should add a message to the array of messages held in state, or return the current state. Finally, create your Redux store and pass it the reducer. */

---
+++ ReactandReduxExtractStateLogictoRedux.jsx
@@ -0,0 +1,24 @@
+//React and Redux: Extract State Logic to Redux
+/*First, define an action type 'ADD' and set it to a const ADD. Next, define an action creator addMessage() which creates the action to add a message. You'll need to pass a message to this action creator and include the message in the returned action.
+Then create a reducer called messageReducer() that handles the state for the messages. The initial state should equal an empty array. This reducer should add a message to the array of messages held in state, or return the current state. Finally, create your Redux store and pass it the reducer. */
+
+// define ADD, addMessage(), messageReducer(), and store here:
+const ADD = 'ADD';
+function addMessage(message) {
+  return {
+    type: ADD,
+    message
+  }
+}
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD':
+      return [...state, action.message];
+      break;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(messageReducer);
