43e2d9453fd57344954704b452ace7a4104223ba
Joana <joanajteixeira@outlook.com>
2019-05-30 15:45:03 +0100

    React and Redux: Map Dispatch to Props
    /* The code editor provides an action creator called addMessage(). Write the function mapDispatchToProps() that takes dispatch as an argument, then returns an object. The object should have a property submitNewMessage set to the dispatch function, which takes a parameter for the new message to add when it dispatches addMessage().*/

---
+++ ReactandReduxMapDispatchtoProps.jsx
@@ -0,0 +1,19 @@
+//React and Redux: Map Dispatch to Props
+/* The code editor provides an action creator called addMessage(). Write the function mapDispatchToProps() that takes dispatch as an argument, then returns an object. The object should have a property submitNewMessage set to the dispatch function, which takes a parameter for the new message to add when it dispatches addMessage().*/
+
+
+const addMessage = (message) => {
+    return {
+      type: 'ADD',
+      message: message
+    }
+  };
+  
+  // change code below this line
+  function mapDispatchToProps(dispatch) {
+    return {
+      submitNewMessage: (message) => dispatch({ type: 'ADD', message: message })
+  
+    }
+  
+  }
